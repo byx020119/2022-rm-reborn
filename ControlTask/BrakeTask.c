fdb05de3bb2a9c7435c1089704fb917fea697547
白羽 <byx020119@foxmail.com>
2021-12-04 10:46:18 +0800

    刹车调整

--- ControlTask/BrakeTask.c
+++ ControlTask/BrakeTask.c
@@ -14,19 +14,19 @@ void Brake_Handle()
 	//光电管检测刹车
 	if(RC_CtrlData.rc.s2!=1&&RC_CtrlData.rc.s2!=2)//测试以及停止状态下不进此函数
 		{
-			if((Phototube_brake==1||(last_Chassis_Temp_Speed<0&&Chassis_Temp_Speed >0)||(last_Chassis_Temp_Speed<0&&Chassis_Temp_Speed ==0))&&change_brake==0)//左侧检测换向
+			if(((last_Chassis_Temp_Speed<0&&Chassis_Temp_Speed >0)||(last_Chassis_Temp_Speed<0&&Chassis_Temp_Speed ==0))&&change_brake==0)//左侧检测换向
 			{
 				Brake_flag=1;
-				Phototube_brake=0;//光电管检测刹车标志清零
+
 				change_brake=1;
 				
 				if(brake_limt.time_error==0)brake_limt.time_last=Get_Time_Micros();		 
 		
 			}
-			if((Phototube_brake1==1||(last_Chassis_Temp_Speed>0&&Chassis_Temp_Speed <0)||(last_Chassis_Temp_Speed>0&&Chassis_Temp_Speed ==0))&&change_brake==0)//右侧检测换向
+			if(((last_Chassis_Temp_Speed>0&&Chassis_Temp_Speed <0)||(last_Chassis_Temp_Speed>0&&Chassis_Temp_Speed ==0))&&change_brake==0)//右侧检测换向
 			{
 				Brake_flag=-1;
-				Phototube_brake=0;
+			
 				change_brake=1;
 				
 				if(brake_limt.time_error==0)brake_limt.time_last=Get_Time_Micros();
