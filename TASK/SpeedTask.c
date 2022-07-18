23f7c0c94fce51c176c7e527c1d4ed28932b2cce
unknown <byx020119@foxmail.com>
2022-07-18 19:06:15 +0800

    训练微调

--- TASK/SpeedTask.c
+++ TASK/SpeedTask.c
@@ -206,8 +206,8 @@ void GMYawControlLoop(void)
 		GMYPositionPID.ki = 0.005*exp(-0.3*fabs(GMYPositionPID.ref - GMYPositionPID.fdb));//0.1;//0.005;//0.01*exp(-0.3*fabs(GMYPositionPID.ref - GMYPositionPID.fdb));//0.1//5
 		GMYPositionPID.kd = 0;//5;//-5*(1-exp(-0.3*fabs(GMYPositionPID.ref - GMYPositionPID.fdb)));//10-5//3;//0;
 		
-		GMYSpeedPID.kp = 65;//65;//10//15
-		GMYSpeedPID.ki = 0.001;// 0.001;//0.005;
+		GMYSpeedPID.kp = 80;//65;//10//15
+		GMYSpeedPID.ki = 0;// 0.001;//0.005;
 		GMYSpeedPID.kd = 0;// 2;//1//2//5
 		
 		GMYPositionPID.Calc(&GMYPositionPID);    //得到yaw轴位置环输出控制量
